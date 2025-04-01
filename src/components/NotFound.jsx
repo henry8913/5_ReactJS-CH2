eb29ae968fd5af885b796b538ce4017726575c3d
“henry8913” <tarantola3.0>
2025-04-01 21:48:03 +0200

    0.8

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <Container className="text-center py-5 mt-5">
+    <Container className="text-center py-5">
       <h1 className="display-1">404</h1>
       <h2 className="mb-4">Pagina Non Trovata</h2>
       <p className="lead mb-4">La pagina che stai cercando non esiste.</p>
