5b4f5af4bb57239f810b994bab8178d61c74f566
“henry8913” <tarantola3.0>
2025-04-01 21:48:49 +0200

    0.8

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,13 +7,13 @@ const NotFound = () => {
   return (
     <Container className="text-center py-5">
       <h1 className="display-1">404</h1>
-      <h2 className="mb-4">Pagina Non Trovata</h2>
-      <p className="lead mb-4">La pagina che stai cercando non esiste.</p>
+      <h2 className="mb-4">Page Not Found</h2>
+      <p className="lead mb-4">The page you are looking for does not exist.</p>
       <Link to="/" className="btn btn-primary">
-        Torna alla Home
+      Return to Home
       </Link>
     </Container>
-  );
+    );
 };
 
 export default NotFound;
